8184e11f9b29b31b4c81f54a51510bf0cf4a33fe
KingGoose20 <41673777+KingGoose20@users.noreply.github.com>
2023-06-25 17:37:41 +1000

    Videos

--- js/Scroll.js
+++ js/Scroll.js
@@ -1,7 +1,7 @@
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
-if (prevScrollpos > currentScrollPos && window.innerWidth < 1251) {
+if ((prevScrollpos > currentScrollPos && window.innerWidth < 1251) || document.getElementsByClassName("nav-menu")[0].classList.contains("active")) {
     document.getElementById("Whole").style.top = "0";
 } else if ((prevScrollpos+ 20) < currentScrollPos && window.innerWidth < 1251) {
     offset = "-" + document.getElementById("Whole").offsetHeight + "px"
--- js/index.js
+++ js/index.js
@@ -1,22 +1,28 @@
 var myVid = document.getElementById("backgroundVideo");
-  switch (Math.floor(Math.random() * 5)) {
+  switch (Math.floor(Math.random() * 7)) {
   case 0:
-    myVid.poster = "Images/Poster/Decider Game.png"
-    myVid.src = "Videos/Decider Game.mp4"
-    break;
-  case 1:
-    myVid.poster = "Images/Poster/Part 1.png"
+    myVid.poster = "Images/Poster/Part 1.jpg"
     myVid.src = "Videos/Part 1.mp4"
     break;
-  case 2:
-    myVid.poster = "Images/Poster/Part 2.png"
+  case 1:
+    myVid.poster = "Images/Poster/Part 2.jpg"
     myVid.src = "Videos/Part 2.mp4"
     break;
-  case 3:
-    myVid.poster = "Images/Poster/Part 3.png"
+  case 2:
+    myVid.poster = "Images/Poster/Part 3.jpg"
     myVid.src = "Videos/Part 3.mp4"
     break;
-  case 4:
-    myVid.poster = "Images/Poster/Part 4.png"
+  case 3:
+    myVid.poster = "Images/Poster/Part 4.jpg"
     myVid.src = "Videos/Part 4.mp4"
+    break;
+  case 4:
+    myVid.poster = "Images/Poster/Part 5.jpg"
+    myVid.src = "Videos/Part 5.mp4"
+  case 5:
+    myVid.poster = "Images/Poster/Part 6.jpg"
+    myVid.src = "Videos/Part 6.mp4"
+  case 6:
+    myVid.poster = "Images/Poster/Part 7.jpg"
+    myVid.src = "Videos/Part 7.mp4"
 }
